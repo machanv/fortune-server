066d80a6274ae514e563f34dec34f8cc87f04e0e
machanv <viola_ma2017@hotmail.com>
2021-06-24 18:18:21 +0800

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Koa = require("koa");
 const fs = require("fs");
 const Router = require("koa-router");
 const views = require("koa-views");
+const db = require("db");
 
 const app = new Koa();
 const port = 3000;
@@ -13,10 +14,15 @@ app.use(views(__dirname + "/views", {
     html: "pug",
   },
 }));
+app.context.db = db();
 router.get('/',async (ctx) => {
   ctx.set("content-type", "text/html;charset=utf8");
   // ctx.body = "Hello World";
   await ctx.render('index.pug');
+  console.log(ctx.db);
 });
 app.use(router.routes());
+app.on('error',err=>{
+  log.err('server err',err);
+})
 app.listen(3000);
