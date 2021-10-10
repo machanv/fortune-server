5707cd3ce3b57c611ffcd9e95bad8f2c9c01fb9b
machanv <viola_ma2017@hotmail.com>
2021-10-10 23:21:29 +0800

    update

---
+++ models/index.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// const schema = new mongoose.Schema({
+//   p1: String,
+//   p2: String
+// });
+
+// const Obj = mongoose.model('names', schema);
+
+// system user
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String
+})
+
+const User = mongoose.model('users', userSchema)
+
+const docSchema = new mongoose.Schema({
+  name: String
+})
+const Doc = mongoose.model('doc', docSchema);
+
+module.exports = {
+  User,
+  Doc
+}
