4b650e3b1a0c41cfe80f974a8cd42d45eb6b46ca
machanv <viola_ma2017@hotmail.com>
2023-03-10 17:02:17 +0800

    role and permission model

--- src/model/comment.model.js
+++ src/model/comment.model.js
@@ -5,12 +5,18 @@ const seq = require('../db/seq');
 const User = require('./user.model');
 const Article = require('./article.model');
 
+// 评论
 const Comment = seq.define('mc_comment', {
   name: {
     type: DataTypes.STRING,
     allowNull: false,
     comment: '标题',
   },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    comment: '用户id，发评论的人',
+  },
 });
-
+Comment.sync({ force: true });
 module.exports = Comment;
---
+++ src/model/permission.model.js
@@ -0,0 +1,10 @@
+const { DataTypes } = require('sequelize');
+const seq = require('../db/seq');
+
+const Permission = seq.define('mc_permission', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    comment: '权限名称',
+  },
+});
